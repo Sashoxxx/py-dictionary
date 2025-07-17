01c2e099e5d20279b8b539af49c4469ef3223f2b
Oleksandr Pivtorak <Sanjok150397@gmail.com>
2025-07-17 21:26:44 +0300

    solution

--- app/main.py
+++ app/main.py
@@ -1,2 +1,78 @@
+from __future__ import annotations
+from typing import Hashable, Iterator
+
+
 class Dictionary:
-    pass
+    def __init__(self) -> None:
+        self.capacity = 8
+        self.table = [[] for _ in range(self.capacity)]
+        self.increase_factor = 0.66
+        self.size = 0
+
+    def __setitem__(self, key: Hashable, value: any) -> None:
+        hash_of_key = hash(key)
+        hash_idx = hash_of_key % self.capacity
+        for idx, (element, _) in enumerate(self.table[hash_idx]):
+            if element == key:
+                self.table[hash_idx][idx] = (key, value)
+                return
+
+        if self.size >= self.capacity * self.increase_factor:
+            self.__resize()
+            hash_idx = hash(key) % self.capacity
+
+        self.table[hash_idx].append((key, value))
+        self.size += 1
+
+    def __getitem__(self, key: Hashable) -> any:
+        hash_of_key = hash(key)
+        hash_idx = hash_of_key % self.capacity
+        for element, value in self.table[hash_idx]:
+            if element == key:
+                return value
+        raise KeyError
+
+    def get(self, key: Hashable) -> any:
+        hash_value = hash(key)
+        hash_idx = hash_value % self.capacity
+        for element, value in self.table[hash_idx]:
+            if element == key:
+                return value
+        return None
+
+    def pop(self, key: Hashable) -> any:
+        value = self.get(key)
+        if value is not None:
+            self.__delitem__(key)
+        return value
+
+    def __iter__(self) -> Iterator[Hashable]:
+        for element in self.table:
+            for key, _ in element:
+                yield key
+
+    def __delitem__(self, key: Hashable) -> None:
+        hash_of_key = hash(key)
+        hash_idx = hash_of_key % self.capacity
+        for idx, (element, value) in enumerate(self.table[hash_idx]):
+            if element == key:
+                del self.table[hash_idx][idx]
+                self.size -= 1
+                return
+        raise KeyError
+
+    def clear(self) -> None:
+        self.__init__()
+
+    def __resize(self) -> None:
+        old_table = self.table[:]
+        self.capacity *= 2
+        self.table = [[] for _ in range(self.capacity)]
+        self.size = 0
+
+        for chunk in old_table:
+            for key, value in chunk:
+                self[key] = value
+
+    def __len__(self) -> int:
+        return self.size
